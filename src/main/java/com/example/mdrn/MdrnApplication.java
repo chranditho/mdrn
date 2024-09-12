e98c09756cb4efc82c307288a804826474a17ed5
christophthonhauser <c.thonhauser97@gmail.com>
2024-09-12 14:49:54 +0200

    init commit

---
+++ src/main/java/com/example/mdrn/MdrnApplication.java
@@ -0,0 +1,13 @@
+package com.example.mdrn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdrnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdrnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mdrn/MdrnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mdrn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdrnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
