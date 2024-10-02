e8d737f865c5c4e57a7563744966e53d6f38dded
christophthonhauser <c.thonhauser97@gmail.com>
2024-10-02 10:36:34 +0200

    add Plant test and fix BL

--- src/main/java/com/example/mdrn/plants/domain/model/Plant.java
+++ src/main/java/com/example/mdrn/plants/domain/model/Plant.java
@@ -1,8 +1,9 @@
 package com.example.mdrn.plants.domain.model;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
-public record Plant(Long id, String name, String species, LocalDate plantedDate) {
+public record Plant(UUID id, String name, String species, LocalDate plantedDate) {
   public boolean isMature() {
     return plantedDate.isBefore(LocalDate.now().minusYears(1));
   }
--- src/test/java/com/example/mdrn/plants/domain/model/PlantTest.java
+++ src/test/java/com/example/mdrn/plants/domain/model/PlantTest.java
@@ -3,19 +3,21 @@ package com.example.mdrn.plants.domain.model;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 
 class PlantTest {
 
   @Test
   void shouldBeImmatureIfPlantAgeIsLessThanOneYear() {
-    Plant plant = new Plant(1L, "Rose", "Rosa", LocalDate.now().minusMonths(11));
+    Plant plant = new Plant(UUID.randomUUID(), "Rose", "Rosa", LocalDate.now().minusMonths(11));
     assertFalse(plant.isMature());
   }
 
   @Test
   void shouldBeMatureIfPlantAgeIsMoreThanOneYear() {
-    Plant plant = new Plant(1L, "Rose", "Rosa", LocalDate.now().minusYears(1).minusDays(1));
+    Plant plant =
+        new Plant(UUID.randomUUID(), "Rose", "Rosa", LocalDate.now().minusYears(1).minusDays(1));
     assertTrue(plant.isMature());
   }
 }
