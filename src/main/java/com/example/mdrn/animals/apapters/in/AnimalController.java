e469a7e72edfe86a4045d03f86c7d4e3768e8157
christophthonhauser <c.thonhauser97@gmail.com>
2025-01-19 15:40:20 +0100

    simplify animal controller

--- src/main/java/com/example/mdrn/animals/apapters/in/AnimalController.java
+++ src/main/java/com/example/mdrn/animals/apapters/in/AnimalController.java
@@ -3,19 +3,17 @@ package com.example.mdrn.animals.apapters.in;
 import com.example.mdrn.animals.domain.model.Animal;
 import com.example.mdrn.animals.ports.in.AnimalPort;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/animals")
 public class AnimalController {
   private final AnimalPort animalPort;
-
-  public AnimalController(AnimalPort animalPort) {
-    this.animalPort = animalPort;
-  }
 
   @GetMapping("/herbivores")
   public ResponseEntity<List<Animal>> getAllHerbivores() {
